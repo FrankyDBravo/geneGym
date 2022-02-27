bffa729d48f68f3b6c611367adea4e882e1b390d
FrankyDBravo <tom.suter.j@gmail.com>
2022-02-27 13:07:25 +0100

    added models

--- website/geneGym/gymProgram/models.py
+++ website/geneGym/gymProgram/models.py
@@ -1,3 +1,52 @@
 from django.db import models
 
 # Create your models here.
+
+class muscle():
+
+    muscle_id = models.ForeignKey( )
+    name = models.CharField()
+    is_front = models.BooleanField()
+    image_url_main =  models.URLField()
+    image_url_secondary =  models.URLField()
+
+
+class exercise():
+
+    id = models.IntegerField(primary_key=True)
+    uuid = models.CharField()
+    name = models.CharField()
+    status	= models.CharField()
+    description	 = models.CharField()
+    creation_date	= models.DateField()
+    category = models.ForeignKey()
+    language = models.ForeignKey()
+    images = models.URLField()
+    license	= models.CharField
+    license_author	= models.CharField
+
+class exerciseImage():
+
+    exercise_id = models.ForeignKey( )
+    image = models.URLField()
+    isMain = models.BooleanField()
+
+class exerciseVariation():
+    
+    exercise_id = models.ForeignKey( )
+    exercise_variation_id = models.ForeignKey( )
+
+class exercise2muscle():
+
+    exercise_id = models.ForeignKey( )
+    muscle_id = models.ForeignKey()
+
+class exercise2muscleSecondary():
+
+    exercise_id = models.ForeignKey( )
+    muscle_id = models.ForeignKey()
+
+class exercise2equipment():
+
+    exercise_id = models.ForeignKey( )
+    equipment_id = models.ForeignKey( )
